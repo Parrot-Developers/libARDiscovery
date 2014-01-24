423a00f20093b51878b5a33b34efb04e491c2560
Frédéric D'HAEYER <frederic.dhaeyer@parrot.com>
2014-01-24 10:46:34 +0100

    Add missing file.

---
+++ Includes/libARDiscovery/ARDiscovery.h
@@ -0,0 +1,8 @@
+#ifndef _ARDISCOVERY_H_
+#define _ARDISCOVERY_H_
+
+#include <libARDiscovery/ARDISCOVERY_Connection.h>
+#include <libARDiscovery/ARDISCOVERY_Discovery.h>
+#include <libARDiscovery/ARDISCOVERY_Error.h>
+
+#endif /* _ARDISCOVERY_H_ */
