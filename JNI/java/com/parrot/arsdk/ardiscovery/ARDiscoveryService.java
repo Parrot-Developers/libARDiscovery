d1319bf10331e8c82d30f3893ea05f4160ab9cf4
m.maitre MAITRE <maxime.maitre@parrot.com>
2014-11-06 10:18:06 +0100

    Revert "[DEV](Android) implement stop and restart for nsd discovery"
    This reverts commit 88613c4c2ca7b50f0c208110033ee4c852e54bda.

--- JNI/java/com/parrot/arsdk/ardiscovery/ARDiscoveryService.java
+++ JNI/java/com/parrot/arsdk/ardiscovery/ARDiscoveryService.java
@@ -260,14 +260,6 @@ public class ARDiscoveryService extends Service
             wifiDiscovery.stop();
         }
     }
-
-    public synchronized void restartNSDDiscovering()
-    {
-        if (wifiDiscovery instanceof ARDiscoveryNsdDiscovery)
-        {
-            ((ARDiscoveryNsdDiscovery)wifiDiscovery).restart();
-        }
-    }
     
     public synchronized void startBLEDiscovering()
     {
