e29a1e64bc2f2271dff86c24efa9fdb6356cb6d4
m.maitre <maxime.maitre@parrot.com>
2015-02-04 18:45:36 +0100

    Revert "[DEV] add json key ARSDK_ARCOMMANDS_VERSION"
    This reverts commit 9db9ccb0e281df90611fd46de9e039cdc6eecc2c.

--- JNI/c/ARDISCOVERY_JNI_Connection.c
+++ JNI/c/ARDISCOVERY_JNI_Connection.c
@@ -280,16 +280,6 @@ Java_com_parrot_arsdk_ardiscovery_ARDiscoveryConnection_nativeGetDefineJsonSkyCo
 }
 
 /**
- * @brief get ARDISCOVERY_CONNECTION_JSON_ARSDK_ARCOMMANDS_VERSION
- * @return value of ARDISCOVERY_CONNECTION_JSON_ARSDK_ARCOMMANDS_VERSION
- */
-JNIEXPORT jstring JNICALL
-Java_com_parrot_arsdk_ardiscovery_ARDiscoveryConnection_nativeGetDefineJsonARSDKARCommandsVersionKey (JNIEnv *env, jclass class)
-{
-    return  (*env)->NewStringUTF(env, ARDISCOVERY_CONNECTION_JSON_ARSDK_ARCOMMANDS_VERSION);
-}
-
-/**
  * @brief get ARDISCOVERY_CONNECTION_TX_BUFFER_SIZE
  * @return value of ARDISCOVERY_CONNECTION_TX_BUFFER_SIZE
  */
