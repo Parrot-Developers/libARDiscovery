aee98f55e38075932a8de8834bef536600012eee
florian leprince <florian.leprince.ext@parrot.com>
2015-02-20 13:48:01 +0100

    Add json data in published service
    * json containing serial number
    
    Change-Id: Id0415baf1442850fb2564278521db73b316ed395

--- Sources/ARDISCOVERY_AvahiDiscovery_nodbus.h
+++ Sources/ARDISCOVERY_AvahiDiscovery_nodbus.h
@@ -8,7 +8,7 @@
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in
-      the documentation and/or other materials provided with the 
+      the documentation and/or other materials provided with the
       distribution.
     * Neither the name of Parrot nor the names
       of its contributors may be used to endorse or promote products
@@ -22,7 +22,7 @@
     COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
     BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-    OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+    OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
     AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
     OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
     OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
@@ -47,6 +47,7 @@ struct ARDISCOVERY_AvahiDiscovery_PublisherData_t
 {
     char* serviceName;              // Specific to each device ("ARDrone_21452365")
     char* serviceType;              // Specific to each platform ("_ardrone3._ucp")
+    char* serviceJsonData;          // Custom data
     uint32_t devicePort;            // Port advertised by device
 };
 
