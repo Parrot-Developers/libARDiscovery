e670771131b817563d1a741650de8222af8b6e6d
Nicolas BRULEZ <nicolas.brulez@parrot.com>
2015-08-18 15:09:40 +0200

    [DEV] Added ARDISCOVERY_PRODUCT_BEBOP_2 to ARDISCOVERY_getProductFamily
    Change-Id: Ie8e591d459b57c9468feafd3fe4db5e5a71b0fa9

--- Sources/ARDISCOVERY_Discovery.c
+++ Sources/ARDISCOVERY_Discovery.c
@@ -1,32 +1,32 @@
 /*
-    Copyright (C) 2014 Parrot SA
+  Copyright (C) 2014 Parrot SA
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in
-      the documentation and/or other materials provided with the 
-      distribution.
-    * Neither the name of Parrot nor the names
-      of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written
-      permission.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+  * Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in
+  the documentation and/or other materials provided with the
+  distribution.
+  * Neither the name of Parrot nor the names
+  of its contributors may be used to endorse or promote products
+  derived from this software without specific prior written
+  permission.
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-    OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
-    AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-    OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-    SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+  SUCH DAMAGE.
 */
 #include <libARDiscovery/ARDISCOVERY_Discovery.h>
 #include <string.h>
@@ -93,7 +93,7 @@ const char* ARDISCOVERY_getProductName(eARDISCOVERY_PRODUCT product)
         name = "";
     else
         name = (char *)ARDISCOVERY_Discovery_ProductNameTable[product];
-        
+
     return (const char *)name;
 }
 
@@ -104,7 +104,7 @@ void ARDISCOVERY_getProductPathName(eARDISCOVERY_PRODUCT product, char *buffer, 
         const char *name = ARDISCOVERY_getProductName(product);
         int nameLen = strlen(name);
         char *index;
-        
+
         if (length > nameLen)
         {
             strncpy(buffer, name, nameLen + 1);
@@ -134,7 +134,7 @@ eARDISCOVERY_PRODUCT ARDISCOVERY_getProductFromName(const char *name)
         if(strcmp(name, ARDISCOVERY_Discovery_ProductNameTable[i]) == 0)
             product = i;
     }
-    
+
     return product;
 }
 
@@ -150,36 +150,37 @@ eARDISCOVERY_PRODUCT ARDISCOVERY_getProductFromProductID(uint16_t productID)
             product = i;
         }
     }
-    
+
     return product;
 }
 
 eARDISCOVERY_PRODUCT_FAMILY ARDISCOVERY_getProductFamily(eARDISCOVERY_PRODUCT product)
 {
     eARDISCOVERY_PRODUCT_FAMILY family = ARDISCOVERY_PRODUCT_FAMILY_MAX;
-    
+
     switch (product)
     {
-        case ARDISCOVERY_PRODUCT_ARDRONE:
-            family = ARDISCOVERY_PRODUCT_FAMILY_ARDRONE;
-            break;
-        case ARDISCOVERY_PRODUCT_JS:
-        case ARDISCOVERY_PRODUCT_JS_EVO_LIGHT:
-        case ARDISCOVERY_PRODUCT_JS_EVO_RACE:
-            family = ARDISCOVERY_PRODUCT_FAMILY_JS;
-            break; 
-        case ARDISCOVERY_PRODUCT_SKYCONTROLLER:
-            family = ARDISCOVERY_PRODUCT_FAMILY_SKYCONTROLLER;
-            break;
-        case ARDISCOVERY_PRODUCT_MINIDRONE:
-        case ARDISCOVERY_PRODUCT_MINIDRONE_EVO_LIGHT:
-        case ARDISCOVERY_PRODUCT_MINIDRONE_EVO_BRICK:
-        case ARDISCOVERY_PRODUCT_MINIDRONE_EVO_HYDROFOIL:
-            family = ARDISCOVERY_PRODUCT_FAMILY_MINIDRONE;
-            break;
-        default:
-            break;
+    case ARDISCOVERY_PRODUCT_ARDRONE:
+    case ARDISCOVERY_PRODUCT_BEBOP_2:
+        family = ARDISCOVERY_PRODUCT_FAMILY_ARDRONE;
+        break;
+    case ARDISCOVERY_PRODUCT_JS:
+    case ARDISCOVERY_PRODUCT_JS_EVO_LIGHT:
+    case ARDISCOVERY_PRODUCT_JS_EVO_RACE:
+        family = ARDISCOVERY_PRODUCT_FAMILY_JS;
+        break;
+    case ARDISCOVERY_PRODUCT_SKYCONTROLLER:
+        family = ARDISCOVERY_PRODUCT_FAMILY_SKYCONTROLLER;
+        break;
+    case ARDISCOVERY_PRODUCT_MINIDRONE:
+    case ARDISCOVERY_PRODUCT_MINIDRONE_EVO_LIGHT:
+    case ARDISCOVERY_PRODUCT_MINIDRONE_EVO_BRICK:
+    case ARDISCOVERY_PRODUCT_MINIDRONE_EVO_HYDROFOIL:
+        family = ARDISCOVERY_PRODUCT_FAMILY_MINIDRONE;
+        break;
+    default:
+        break;
     }
-    
+
     return family;
 }
