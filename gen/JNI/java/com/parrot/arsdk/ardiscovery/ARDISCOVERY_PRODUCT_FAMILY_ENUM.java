7ae71d732576b3fd2640267df87b43b34e16a49e
Djavan Bertrand <djavan.bertrand@parrot.com>
2015-10-01 11:27:19 +0200

    [DEV] Added Unknown_Product_1 enum
    Change-Id: Ibe05da8d716263d2654bda0b6d5ed333c3baa180
    Reviewed-on: https://gerrit.parrot.biz/22070
    Reviewed-by: Nicolas Brulez <nicolas.brulez@parrot.com>

--- gen/JNI/java/com/parrot/arsdk/ardiscovery/ARDISCOVERY_PRODUCT_FAMILY_ENUM.java
+++ gen/JNI/java/com/parrot/arsdk/ardiscovery/ARDISCOVERY_PRODUCT_FAMILY_ENUM.java
@@ -51,8 +51,10 @@ public enum ARDISCOVERY_PRODUCT_FAMILY_ENUM {
     ARDISCOVERY_PRODUCT_FAMILY_SKYCONTROLLER (2, "SKY CONTROLLER product family"),
    /** DELOS product */
     ARDISCOVERY_PRODUCT_FAMILY_MINIDRONE (3, "DELOS product"),
+   /** Unknown Product 1 product family */
+    ARDISCOVERY_PRODUCT_FAMILY_UNKNOWN_PRODUCT_1 (4, "Unknown Product 1 product family"),
    /** Max of product familys */
-    ARDISCOVERY_PRODUCT_FAMILY_MAX (4, "Max of product familys");
+    ARDISCOVERY_PRODUCT_FAMILY_MAX (5, "Max of product familys");
 
     private final int value;
     private final String comment;
