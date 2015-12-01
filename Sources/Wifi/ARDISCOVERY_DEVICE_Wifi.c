073789007d17261534d89a9f6d9bc1adbaed4803
Djavan Bertrand <djavan.bertrand@parrot.com>
2015-12-01 15:09:25 +0100

    [DEBUG] Fixed support for evos
    Change-Id: I26ea41b7c240e14ff549c5dc6e7f8ea6ae73ae07
    Reviewed-on: https://gerrit.parrot.biz/25827
    Reviewed-by: Maxime Maitre <maxime.maitre@parrot.com>

--- Sources/Wifi/ARDISCOVERY_DEVICE_Wifi.c
+++ Sources/Wifi/ARDISCOVERY_DEVICE_Wifi.c
@@ -552,7 +552,7 @@ eARDISCOVERY_ERROR ARDISCOVERY_DEVICE_Wifi_InitJumpingSumoNetworkConfiguration (
     
     // check parameters
     if ((device == NULL) || 
-        (device->productID != ARDISCOVERY_PRODUCT_JS) ||
+        (ARDISCOVERY_getProductFamily(device->productID) != ARDISCOVERY_PRODUCT_FAMILY_JS) ||
         (networkConfiguration == NULL))
     {
         error = ARDISCOVERY_ERROR_BAD_PARAMETER;
