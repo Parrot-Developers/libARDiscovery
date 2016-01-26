640189d7e4c08ffd73bc4c2041319debeec8fbb2
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-01-26 14:04:50 +0100

    [BUILD] Do not use autotools anymore, just alchemy build
    Change-Id: Ia73d83e77bf90c92617720e50d8e62de68609b9c
    Reviewed-on: https://gerrit.parrot.biz/28568
    Reviewed-by: Yves-Marie Morgan <yves-marie.morgan@parrot.com>

--- atom.mk
+++ atom.mk
@@ -2,61 +2,59 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARDiscovery
 LOCAL_DESCRIPTION := ARSDK Discovery and Connection Management Layer
+LOCAL_CATEGORY_PATH := dragon/libs
 
-LOCAL_LIBRARIES := ARSDKBuildUtils libARSAL libARNetwork libARNetworkAL json
+LOCAL_MODULE_FILENAME := libardiscovery.so
+
+LOCAL_LIBRARIES := \
+	libARSAL \
+	libARNetwork \
+	libARNetworkAL \
+	json
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/Includes \
+	$(LOCAL_PATH)/Sources
+
+LOCAL_SRC_FILES := \
+	Sources/ARDISCOVERY_Connection.c \
+	Sources/ARDISCOVERY_Discovery.c \
+	Sources/ARDISCOVERY_Device.c \
+	Sources/Wifi/ARDISCOVERY_DEVICE_Wifi.c \
+	Sources/BLE/ARDISCOVERY_DEVICE_Ble.c \
+	gen/Sources/ARDISCOVERY_Error.c
+
+LOCAL_INSTALL_HEADERS := \
+	Includes/libARDiscovery/ARDiscovery.h:usr/include/libARDiscovery/ \
+	Includes/libARDiscovery/ARDISCOVERY_Connection.h:usr/include/libARDiscovery/ \
+	Includes/libARDiscovery/ARDISCOVERY_Discovery.h:usr/include/libARDiscovery/ \
+	Includes/libARDiscovery/ARDISCOVERY_NetworkConfiguration.h:usr/include/libARDiscovery/ \
+	Includes/libARDiscovery/ARDISCOVERY_Device.h:usr/include/libARDiscovery/ \
+	Includes/libARDiscovery/ARDISCOVERY_Error.h:usr/include/libARDiscovery/
 
 ifndef ARSDK_BUILD_FOR_APP
+
+# Embedded: Use avahi without dbus
+LOCAL_SRC_FILES += Sources/ARDISCOVERY_AvahiDiscovery_nodbus.c
+LOCAL_INSTALL_HEADERS += Includes/libARDiscovery/ARDISCOVERY_AvahiDiscovery.h:usr/include/libARDiscovery/
+
+else ifeq ("$(TARGET_OS)-$(TARGET_OS_FLAVOUR)","linux-native")
+
+# Native linux: use avahi with dbus
 LOCAL_LIBRARIES += avahi
+LOCAL_SRC_FILES += Sources/ARDISCOVERY_AvahiDiscovery.c
+LOCAL_INSTALL_HEADERS += Includes/libARDiscovery/ARDISCOVERY_AvahiDiscovery.h:usr/include/libARDiscovery/
+
+else ifeq ("$(TARGET_OS)","darwin")
+
+ifneq ("$(TARGET_OS_FLAVOUR)","native")
+# Darwin: use bonjour
+LOCAL_SRC_FILES += Sources/ARDISCOVERY_BonjourDiscovery.m
+LOCAL_INSTALL_HEADERS += Includes/libARDiscovery/ARDISCOVERY_BonjourDiscovery.h:usr/include/libARDiscovery/
 endif
-
-LOCAL_EXPORT_LDLIBS := -lardiscovery
-
-# Copy in build dir so bootstrap files are generated in build dir
-LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
-
-# Configure script is not at the root
-LOCAL_AUTOTOOLS_CONFIGURE_SCRIPT := Build/configure
-
-#Autotools variables
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--with-libARSALInstallDir="" \
-	--with-libARNetworkALInstallDir="" \
-	--with-libARNetworkInstallDir="" \
-	--with-jsonInstallDir=""
-
-ifeq ("$(TARGET_OS_FLAVOUR)","android")
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--disable-static \
-	--enable-shared \
-	--disable-so-version
-
-# Temporary fix to Android build on Mac
-LOCAL_AUTOTOOLS_CONFIGURE_ENV := \
-	ac_cv_objc_compiler_gnu=no
-
-else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--enable-static \
-	--disable-shared \
-	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)" \
-	CFLAGS=" -std=gnu99 -x c $(TARGET_GLOBAL_CFLAGS)"
-
-else ifneq ("$(TARGET_OS_FLAVOUR)","native")
-  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --enable-avahi-nodbus
-  LOCAL_AUTOTOOLS_CONFIGURE_ENV := ac_cv_header_avahi_client_client_h=no
 
 endif
 
-# User define command to be launch before configure step.
-# Generates files used by configure
-define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
-endef
-
-include $(BUILD_AUTOTOOLS)
+include $(BUILD_LIBRARY)
