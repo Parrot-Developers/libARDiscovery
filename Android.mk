1607b7c35ded04167f644292cdf1c0608885110a
Harold Magniez <harold.magniez@parrot.com>
2016-03-02 09:37:49 +0100

    [DEBUG] (Android) Fix galaxy tab crash: Use armv7a instead of armv7a-neon
    Change-Id: I383223b87e5712d72ff89357bb334b71614a96eb
    Reviewed-on: https://gerrit.parrot.biz/31490
    Reviewed-by: Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>

--- Android.mk
+++ Android.mk
@@ -4,9 +4,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS := -g -I$(LOCAL_PATH)/Sources
-ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-    LOCAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon
-endif
 LOCAL_MODULE := libardiscovery_android
 LOCAL_SRC_FILES := JNI/c/ARDISCOVERY_JNI_Connection.c JNI/c/ARDISCOVERY_JNI_Device.c JNI/c/ARDISCOVERY_JNI_Discovery.c JNI/c/ARDISCOVERY_JNI_DEVICE_Ble.c
 LOCAL_LDLIBS := -llog -lz
