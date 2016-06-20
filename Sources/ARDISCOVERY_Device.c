f7ccd9c955b2647c47dffc955b973c80ab4727d0
Maxime MAITRE <maxime.maitre@parrot.com>
2016-06-20 15:10:44 +0200

    [DEV] Add audio part.
    - Add audio stream buffers configurations.
    - Split classic JS and JS evo configuration.
    
    Change-Id: I87a497b7b0ea485ef21dcdd377f60b6afa9cddcc
    Reviewed-on: https://gerrit.parrot.biz/42110
    Reviewed-by: Nicolas Brulez <nicolas.brulez@parrot.com>

--- Sources/ARDISCOVERY_Device.c
+++ Sources/ARDISCOVERY_Device.c
@@ -349,9 +349,11 @@ eARDISCOVERY_ERROR ARDISCOVERY_Device_InitWifi (ARDISCOVERY_Device_t *device, eA
                 device->initNetworkConfiguration = ARDISCOVERY_DEVICE_Wifi_InitBebop2NetworkConfiguration;
                 break;
             case ARDISCOVERY_PRODUCT_JS:
+                device->initNetworkConfiguration = ARDISCOVERY_DEVICE_Wifi_InitJumpingSumoNetworkConfiguration;
+                break;
             case ARDISCOVERY_PRODUCT_JS_EVO_LIGHT:
             case ARDISCOVERY_PRODUCT_JS_EVO_RACE:
-                device->initNetworkConfiguration = ARDISCOVERY_DEVICE_Wifi_InitJumpingSumoNetworkConfiguration;
+                device->initNetworkConfiguration = ARDISCOVERY_DEVICE_Wifi_InitJumpingSumoEvoNetworkConfiguration;
                 break;
             case ARDISCOVERY_PRODUCT_POWER_UP:
                 device->initNetworkConfiguration = ARDISCOVERY_DEVICE_Wifi_InitPowerUpNetworkConfiguration;
